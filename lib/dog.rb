1915d7227bed3dca9a14b583cc73e1eb681b01b5
iamkinglana <ngetichenock32@gmail.com>
2023-05-17 22:41:34 +0300

    First Commit

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,19 @@
 class Dog
+    attr_accessor :name,:breed
+    def initialize=(name,breed)
+        @name= name
+        @breed = breed
+    end
+    
+    def name
+        @name
+    end
 
+    def breed
+        @breed 
+    end
 end
+
+snoopy= Dog.new()
+snoopy.name="snoopy"
+snoopy.breed="Beagle"
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,19 @@
 class Person
+    attr_accessor :name,:job
+    def initialize=(name,job)
+        @name= name
+        @job = job
+    end
 
+    def name
+        @name
+    end
+    def job
+        @job
+    end
 end
+
+singer= Person.new()
+singer.name="Beyonce"
+singer.job="Singer"
+
